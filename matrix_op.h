15b1fff435aa4ca2b46c91f373de2d2906cdbc24
yuwen960130-cmyk <yuwen960130@gmail.com>
2025-12-18 23:43:54 +0800

    basic matrix operations

--- matrix_op.h
+++ matrix_op.h
@@ -2,3 +2,24 @@
 #ifndef MATRIX_OP_H
 #define MATRIX_OP_H
 #define SIZE 3
+
+
+void mat_add(
+    const double A[SIZE][SIZE],
+    const double B[SIZE][SIZE],
+    double C[SIZE][SIZE]
+);// C = A + B 
+
+
+void mat_sub(
+    const double A[SIZE][SIZE],
+    const double B[SIZE][SIZE],
+    double C[SIZE][SIZE]
+);// C = A - B 
+
+void mat_elem_mul(
+    const double A[SIZE][SIZE],
+    const double B[SIZE][SIZE],
+    double C[SIZE][SIZE]
+);
+
