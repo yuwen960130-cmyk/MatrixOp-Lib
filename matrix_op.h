3dbed2d035e4b011efa1e406d0f46a6bda2b6a0b
yuwen960130-cmyk <yuwen960130@gmail.com>
2025-12-18 23:57:26 +0800

    inverse matrix operations

--- matrix_op.h
+++ matrix_op.h
@@ -35,3 +35,24 @@ void mat_transpose(
     double T[SIZE][SIZE]
 );
 
+
+double mat_det2x2(
+    double a11, double a12,
+    double a21, double a22
+);
+
+double mat_det3x3(
+    const double A[SIZE][SIZE]
+);
+
+void mat_adjoint(
+    const double A[SIZE][SIZE],
+    double adj[SIZE][SIZE]
+);
+
+bool mat_inverse(
+    const double A[SIZE][SIZE],
+    double inv[SIZE][SIZE]
+);
+
+
