19f66fcb7cb2ba0f3e335874aff8a7eb0e0e9127
yuwen960130-cmyk <yuwen960130@gmail.com>
2025-12-19 20:43:43 +0800

    Utility Functions

---
+++ matrix_op.c
@@ -0,0 +1,32 @@
+#include "matrix_op.h"
+#include <stdio.h>
+
+
+//Segment 1: Utility Functions
+void mat_copy(const double src[SIZE][SIZE], double dst[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            dst[i][j] = src[i][j];
+        }
+    }
+}
+
+void mat_identity(double I[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            I[i][j] = (i == j) ? 1.0 : 0.0;
+        }
+    }
+}
+
+void mat_print(const char *title, const double A[SIZE][SIZE]) {
+    if (title) printf("%s\n", title);
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%10.4f ", A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
