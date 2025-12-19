f4f4034f16ecef6a5b293f00c9e14a5a23988d48
yuwen960130-cmyk <yuwen960130@gmail.com>
2025-12-19 22:09:51 +0800

    Full Feature Tests

--- main.c
+++ main.c
@@ -16,3 +16,36 @@ static void prepare_matrices(double A[SIZE][SIZE], double B[SIZE][SIZE]) {
     mat_copy(a, A);
     mat_copy(b, B);
 }
+
+//Segment 2: Full Feature Tests
+static void run_full_tests(const double A[SIZE][SIZE], const double B[SIZE][SIZE]) {
+    double C[SIZE][SIZE];
+
+    mat_add(A, B, C);
+    mat_print("A + B =", C);
+
+    mat_mul(A, B, C);
+    mat_print("A * B =", C);
+
+    double T[SIZE][SIZE];
+    mat_transpose(A, T);
+    mat_print("Transpose(A) =", T);
+
+    double detA = mat_det3x3(A);
+    printf("det(A) = %.6f\n\n", detA);
+
+    double adjA[SIZE][SIZE];
+    mat_adjoint(A, adjA);
+    mat_print("adj(A) =", adjA);
+
+    double invA[SIZE][SIZE];
+    if (mat_inverse(A, invA)) {
+        mat_print("inv(A) =", invA);
+
+        double check[SIZE][SIZE];
+        mat_mul(A, invA, check);
+        mat_print("A * inv(A) (should be I) =", check);
+    } else {
+        printf("A is singular (det=0). No inverse.\n\n");
+    }
+}
